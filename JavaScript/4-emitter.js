6a959bc06777921ee91da1543104cee2ce94c960
tshemsedinov <timur.shemsedinov@gmail.com>
2019-04-29 02:56:12 +0300

    Add EventEmitter example

---
+++ JavaScript/4-emitter.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const { EventEmitter } = require('events');
+
+// Usage
+
+const randomChar = () => String
+  .fromCharCode(Math.floor((Math.random() * 25) + 97));
+
+class CharStream { // Observable
+  constructor(ee) {
+    this.timer = setInterval(() => {
+      const char = randomChar();
+      ee.emit('char', char);
+    }, 200);
+  }
+  complete() {
+    clearInterval(this.timer);
+  }
+}
+
+const ee = new EventEmitter();
+const observable = new CharStream(ee);
+
+let count = 0;
+
+const observer = char => {
+  process.stdout.write(char);
+  count++;
+  if (count > 50) {
+    observable.complete();
+    process.stdout.write('\n');
+  }
+};
+
+ee.on('char', observer);
+
+console.dir({ observer, observable });
