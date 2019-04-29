fe51528170a0ee251bdebaf3b3bfafabb2989586
tshemsedinov <timur.shemsedinov@gmail.com>
2019-04-29 19:05:31 +0300

    Implement rx-compatible Observable

---
+++ JavaScript/7-subscriber.js
@@ -0,0 +1,68 @@
+'use strict';
+
+class Observable {
+  constructor(subscribe) {
+    this.observers = [];
+    this.operators = [];
+    if (subscribe) setTimeout(subscribe, 0, this);
+  }
+  subscribe(observer) {
+    this.observers.push(observer);
+    return this;
+  }
+  pipe(...args) {
+    this.operators.push(...args);
+    const destination = new Observable();
+    this.subscribe(data => destination.next(data));
+    return destination;
+  }
+  next(data) {
+    if (this.observers.length === 0) return;
+    for (const operator of this.operators) {
+      if (operator.name === 'filter') {
+        if (!operator.fn(data)) return;
+      }
+      if (operator.name === 'map') {
+        data = operator.fn(data);
+      }
+    }
+    for (const observer of this.observers) {
+      observer(data);
+    }
+  }
+}
+
+const filter = predicate => ({ name: 'filter', fn: predicate });
+const map = callback => ({ name: 'map', fn: callback });
+
+// Usage
+
+const randomChar = () => String
+  .fromCharCode(Math.floor((Math.random() * 25) + 97));
+
+const source = new Observable(subscriber => {
+  setInterval(() => {
+    const char = randomChar();
+    subscriber.next(char);
+  }, 200);
+});
+
+const destination = source.pipe(
+  filter(char => !'aeiou'.includes(char)),
+  map(char => char.toUpperCase())
+);
+
+destination.subscribe(observer);
+
+let count = 0;
+
+function observer(char) {
+  process.stdout.write(char);
+  count++;
+  if (count > 50) {
+    process.stdout.write('\n');
+    process.exit(0);
+  }
+}
+
+console.dir({ observer, source, destination });
